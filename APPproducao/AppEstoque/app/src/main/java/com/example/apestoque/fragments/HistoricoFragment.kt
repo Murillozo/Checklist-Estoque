1da99c2f7bdf20e45c4a9c27991e8cf8dceec82a
Murillozo <130245558+Murillozo@users.noreply.github.com>
2025-08-08 10:03:37 -0300

    Delete APPproducao directory

--- APPproducao/AppEstoque/app/src/main/java/com/example/apestoque/fragments/HistoricoFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.apestoque.fragments
-
-import androidx.fragment.app.Fragment
-import com.example.apestoque.R
-
-class HistoricoFragment : Fragment(R.layout.fragment_historico) {
-    // TODO: exibir histórico de solicitações
-}
