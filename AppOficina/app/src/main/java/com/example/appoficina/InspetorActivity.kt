86a69ee40a9d9abf85707297437a24424cb27a36
Murillozo <130245558+Murillozo@users.noreply.github.com>
2025-08-10 22:21:01 -0300

    feat: implement posto03 pre-montagem checklist

--- AppOficina/app/src/main/java/com/example/appoficina/InspetorActivity.kt
+++ AppOficina/app/src/main/java/com/example/appoficina/InspetorActivity.kt
@@ -18,7 +18,7 @@ class InspetorActivity : AppCompatActivity() {
 
         val fragments: List<Fragment> = listOf(
             Posto02InspetorFragment(),
-            SimpleTextFragment.newInstance("03 - POSTO - 03 PRÉ-MONTAGEM - 01"),
+            Posto03PreMontagemInspetorFragment(),
             SimpleTextFragment.newInstance("04 - POSTO - 04 BARRAMENTO"),
             SimpleTextFragment.newInstance("05 - POSTO - 05 CABLAGEM - 01"),
             SimpleTextFragment.newInstance("06 - POSTO - 06 PRÉ-MONTAGEM - 02"),
